af7ca267a916dfd755dff88bfb78379e4f82e979
Joey Mckenzie <joey.mckenzie27@gmail.com>
2021-09-27 11:21:23 -0700

    initial commit

---
+++ apps/tailwind-dark-mode/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'joey-mckenzie-io-angular-blog-samples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tailwind-dark-mode';
+}
